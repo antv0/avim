9c1c7913f3e52f4c38294c0d3bbe7d4ee097b94e
Antoine Vaure <antoine.vaure@allegrodvt.com>
2023-08-21 13:32:46 +0200

    Add status in lualine for use with noice

--- lua/configs/lualine.lua
+++ lua/configs/lualine.lua
@@ -20,7 +20,13 @@ local opts = {
   sections = {
     lualine_a = { 'mode' },
     lualine_b = { 'branch', 'diff' },
-    lualine_c = { 'filename' },
+    lualine_c = {
+      'filename',
+      {
+        require("noice").api.status.mode.get,
+        cond = require("noice").api.status.mode.has,
+      },
+    },
     lualine_x = { 'diagnostics', 'filetype' },
     lualine_y = { 'progress' },
     lualine_z = { 'location' }
