a0d7fc8bc90a38ed84321befa6517dd82e589824
ant <ant@domain.xyz>
2023-12-08 00:48:20 +0700

    fix neogit deps

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -359,6 +359,7 @@ local plugins = {
       "nvim-lua/plenary.nvim",         -- required
       "nvim-telescope/telescope.nvim", -- optional
       "sindrets/diffview.nvim",        -- optional
+      "ibhagwan/fzf-lua",              -- optional
     },
     config = true
   },
